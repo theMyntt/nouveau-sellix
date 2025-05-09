25d366d908d237149dfa861efbb6c601ae19866c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-08 21:52:18 -0300

    test: password value object unit specs

---
+++ NouveauSellix.Tests/Domain/Users/ValueObjects/PasswordValueObjectTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NouveauSellix.Domain.Users.ValueObjects;
+
+namespace NouveauSellix.Tests.Domain.Users.ValueObjects
+{
+    public class PasswordValueObjectTests
+    {
+        [Fact]
+        public void ShouldGenerateHashAndSalt()
+        {
+            var password = "mypasswordkey";
+            var obj = new PasswordValueObject(password);
+
+            Assert.NotNull(obj.Hash);
+            Assert.NotEmpty(obj.Salt);
+        }
+
+        [Fact]
+        public void ShouldMatchPasswords()
+        {
+            var password = "mypasswordkey";
+            var obj = new PasswordValueObject(password);
+
+            Assert.NotNull(obj.Hash);
+            Assert.NotEmpty(obj.Salt);
+
+            var isEqual = obj.MatchesWith(password);
+
+            Assert.True(isEqual);
+        }
+
+        [Fact]
+        public void ShouldNotMatchPasswords()
+        {
+            var password = "mypasswordkey";
+            var anotherPassword = "mypasskey";
+            var obj = new PasswordValueObject(password);
+
+            Assert.NotNull(obj.Hash);
+            Assert.NotEmpty(obj.Salt);
+
+            var isEqual = obj.MatchesWith(anotherPassword);
+
+            Assert.False(isEqual);
+        }
+    }
+}
