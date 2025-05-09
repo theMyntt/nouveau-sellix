6c123a38656685dc36b8cfda4e8c3dc5cc3c338e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-08 21:54:31 -0300

    test: email value object unit specs

---
+++ NouveauSellix.Tests/Domain/Users/ValueObjects/EmailValueObjectTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NouveauSellix.Domain.Users.ValueObjects;
+
+namespace NouveauSellix.Tests.Domain.Users.ValueObjects
+{
+    public class EmailValueObjectTests
+    {
+        [Fact]
+        public void ShouldCreateEmail()
+        {
+            var email = "john.doe@example.com";
+            var obj = new EmailValueObject(email);
+
+            Assert.Equal(email, obj.Value);
+        }
+
+        [Fact]
+        public void ShouldBlockEmail()
+        {
+            var email = "john.doe";
+
+            Assert.Throws<ArgumentException>(() => new EmailValueObject(email));
+        }
+    }
+}
