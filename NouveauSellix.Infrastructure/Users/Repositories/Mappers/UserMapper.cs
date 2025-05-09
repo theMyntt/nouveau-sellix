f3a14d601c160bea8c72b9112dcc3e9ccd764f1a
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-08 22:56:59 -0300

    feat: user mapper and repository

---
+++ NouveauSellix.Infrastructure/Users/Repositories/Mappers/UserMapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NouveauSellix.Domain.Users.Entities;
+using NouveauSellix.Domain.Users.ValueObjects;
+using NouveauSellix.Infrastructure.Users.Tables;
+
+namespace NouveauSellix.Infrastructure.Users.Repositories.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserTable ToTable(this UserEntity entity)
+        {
+            return new UserTable
+            {
+                Id = entity.Id,
+                Name = entity.Name,
+                Email = entity.Email.Value,
+                PasswordHash = entity.Password.Hash,
+                PasswordSalt = entity.Password.Salt,
+                ImagePath = entity.ImagePath,
+                CreatedAt = entity.CreatedAt,
+                UpdatedAt = entity.UpdatedAt
+            };
+        }
+
+        public static UserEntity ToEntity(this UserTable table)
+        {
+            var email = new EmailValueObject(table.Email);
+            var password = new PasswordValueObject(table.PasswordHash, table.PasswordSalt);
+
+            return new UserEntity(
+                table.Name,
+                email,
+                password,
+                table.IsBlocked,
+                table.CreatedAt,
+                table.Id,
+                table.UpdatedAt,
+                table.ImagePath);
+        }
+    }
+}
---
+++ NouveauSellix.Infrastructure/Users/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NouveauSellix.Application.Users.Abstractions;
+using NouveauSellix.Domain.Users.Entities;
+using NouveauSellix.Domain.Users.ValueObjects;
+using NouveauSellix.Infrastructure.Shared;
+using NouveauSellix.Infrastructure.Users.Repositories.Mappers;
+using NouveauSellix.Infrastructure.Users.Tables;
+
+namespace NouveauSellix.Infrastructure.Users.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DatabaseContext _context;
+        private readonly DbSet<UserTable> _table;
+
+        public UserRepository(DatabaseContext context)
+        {
+            _context = context;
+            _table = _context.Set<UserTable>();
+        }
+
+        public async Task SaveUserAsync(UserEntity user)
+        {
+            var table = user.ToTable();
+
+            await _table.AddAsync(table);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<UserEntity?> SearchByEmailAsync(EmailValueObject email)
+        {
+            var user = await _table.SingleOrDefaultAsync(u => u.Email == email.Value);
+
+            return user?.ToEntity();
+        }
+    }
+}
