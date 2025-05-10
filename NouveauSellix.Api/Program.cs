64ec1e2105b7939bbfb078e2c011b5471e37be85
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-09 22:36:53 -0300

    chore: specify controllers with auth is required

--- NouveauSellix.Api/Program.cs
+++ NouveauSellix.Api/Program.cs
@@ -1,6 +1,9 @@
 
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 using NouveauSellix.Api.Middlewares;
 using NouveauSellix.Infrastructure;
+using NouveauSellix.Api.Filters;
 
 namespace NouveauSellix.Api
 {
@@ -20,7 +23,20 @@ namespace NouveauSellix.Api
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Insira o token JWT no campo: **Bearer {seu token}**"
+                });
+
+                c.OperationFilter<SwaggerAuthFilter>();
+            });
 
             var app = builder.Build();
 
