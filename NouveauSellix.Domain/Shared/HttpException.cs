0b5e52531a283112a9ce5c9efa3870e36fa5e311
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-09 22:13:34 -0300

    chore: http exception shared base

---
+++ NouveauSellix.Domain/Shared/HttpException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NouveauSellix.Domain.Shared
+{
+    public class HttpException : Exception
+    {
+        public int StatusCode { get; private set; }
+
+        public HttpException(string message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
