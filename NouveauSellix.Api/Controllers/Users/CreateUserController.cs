4c95e77e1f21113f2a0820b7d879cbd332091555
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-09 21:52:44 -0300

    feat: create user controller

---
+++ NouveauSellix.Api/Controllers/Users/CreateUserController.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using NouveauSellix.Application.Users.Services.CreateUser;
+using NouveauSellix.Application.Users.Services.CreateUser.IO;
+
+namespace NouveauSellix.Api.Controllers.Users
+{
+    [ApiController]
+    [Route("/api")]
+    [Tags("Create User", "User")]
+    public class CreateUserController : ControllerBase
+    {
+        [HttpPost("v1/user")]
+        [EndpointSummary("Create a new user")]
+        public async Task<IActionResult> PerformV1(
+            [FromBody] CreateUserServiceInput input,
+            [FromServices] ICreateUserService service)
+        {
+            var result = await service.ExecuteAsync(input);
+
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
