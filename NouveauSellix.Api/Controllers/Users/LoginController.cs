ccba375ad8844abf13da9998359a8f56420ee6de
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-09 22:06:35 -0300

    feat: login controller

---
+++ NouveauSellix.Api/Controllers/Users/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NouveauSellix.Application.Users.Services.Login;
+using NouveauSellix.Application.Users.Services.Login.IO;
+
+namespace NouveauSellix.Api.Controllers.Users
+{
+    [ApiController]
+    [Route("/api")]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost("v1/auth")]
+        [EndpointDescription("Generate JWT Token for Auth")]
+        public async Task<IActionResult> PerformV1(
+            [FromBody] LoginServiceInput input,
+            [FromServices] ILoginService service)
+        {
+            var result = await service.ExecuteAsync(input);
+
+            return StatusCode(result.StatusCode, result);
+        }
+    }
+}
