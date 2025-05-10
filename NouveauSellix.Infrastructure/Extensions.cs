2117ce29ad8b3ab58cb6e0b4507bb6596bd3b1ef
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-09 21:40:18 -0300

    feat: add infra extensions

---
+++ NouveauSellix.Infrastructure/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NouveauSellix.Application.Users.Abstractions;
+using NouveauSellix.Infrastructure.Shared;
+using NouveauSellix.Infrastructure.Users.Handlers;
+using NouveauSellix.Infrastructure.Users.Repositories;
+
+namespace NouveauSellix.Infrastructure
+{
+    public static class Extensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var conn = configuration["SqlServer:ConnectionString"] ?? throw new Exception("SqlServer:ConnectionString Is Null");
+
+            services.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseSqlServer(conn, o =>
+                {
+                    o.MigrationsAssembly(typeof(DatabaseContext).Assembly.FullName);
+                });
+            });
+            services.AddScoped<IJwtHandler, JwtHandler>();
+            services.AddScoped<IUserFileManager, UserFileManager>();
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
