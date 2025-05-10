52c7272892688ab97dd1b097395e678242b1d42e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-10 00:16:40 -0300

    feat: update user image controller

---
+++ NouveauSellix.Api/Controllers/Users/UpdateUserImageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NouveauSellix.Application.Users.Services.UpdateUserImage;
+using NouveauSellix.Application.Users.Services.UpdateUserImage.IO;
+
+namespace NouveauSellix.Api.Controllers.Users
+{
+    [Authorize]
+    [ApiController]
+    [Route("/api")]
+    [Tags("Image", "User")]
+    public class UpdateUserImageController : ControllerBase
+    {
+        [HttpPut("v1/user/image/")]
+        [EndpointSummary("Update user image")]
+        public async Task<IActionResult> PerformV1(
+            [FromForm] UpdateUserImageServiceInput input,
+            [FromServices] IUpdateUserImageService service)
+        {
+            await service.ExecuteAsync(input);
+
+            return NoContent();
+        }
+    }
+}
