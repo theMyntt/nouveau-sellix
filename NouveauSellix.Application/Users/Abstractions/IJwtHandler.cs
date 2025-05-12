97e8e0b19d1dea1bc4de1d851d2f44b1ddb84f1c
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-05-12 20:26:26 -0300

    feat: verify the autencity of token

--- NouveauSellix.Application/Users/Abstractions/IJwtHandler.cs
+++ NouveauSellix.Application/Users/Abstractions/IJwtHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using NouveauSellix.Domain.Users.Entities;
@@ -10,5 +11,15 @@ namespace NouveauSellix.Application.Users.Abstractions
     public interface IJwtHandler
     {
         string GenerateToken(UserEntity user);
+
+        /// <summary>
+        /// Verifica a autênticidade do token JWT, se 
+        /// autêntico, retorna suas Claims. Se não, estoura a 
+        /// exceção UnAuthenticJwtTokenException
+        /// </summary>
+        /// <param name="token">String do token JWT</param>
+        /// <returns></returns>
+        /// <exception cref="UnAuthenticJwtTokenException">Token não autêntico ou invalido</exception>
+        ClaimsPrincipal IsAuthentic(string token);
     }
 }
--- NouveauSellix.Infrastructure/Users/Handlers/JwtHandler.cs
+++ NouveauSellix.Infrastructure/Users/Handlers/JwtHandler.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using NouveauSellix.Application.Users.Abstractions;
 using NouveauSellix.Domain.Users.Entities;
+using NouveauSellix.Infrastructure.Users.Handlers.Exceptions;
 
 namespace NouveauSellix.Infrastructure.Users.Handlers
 {
@@ -16,14 +17,15 @@ namespace NouveauSellix.Infrastructure.Users.Handlers
     {
         private readonly SigningCredentials _credentials;
         private readonly IConfigurationSection _jwtSection;
+        private readonly SymmetricSecurityKey _key;
 
         public JwtHandler(IConfiguration configuration)
         {
             var jwtSection = configuration.GetSection("Jwt");
             _jwtSection = jwtSection;
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["SecretKey"]!));
-            _credentials = new(key, SecurityAlgorithms.HmacSha256);
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSection["SecretKey"]!));
+            _credentials = new(_key, SecurityAlgorithms.HmacSha256);
         }
 
         public string GenerateToken(UserEntity user)
@@ -42,6 +44,28 @@ namespace NouveauSellix.Infrastructure.Users.Handlers
                 signingCredentials: _credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public ClaimsPrincipal IsAuthentic(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = false,
+                IssuerSigningKey = _key,
+                ValidIssuer = _jwtSection["Issuer"],
+                ValidAudience = _jwtSection["Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+
+            if (securityToken is not JwtSecurityToken jwtToken || !jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new UnAuthenticJwtTokenException();
+
+            return principal;
         }
     }
 }
