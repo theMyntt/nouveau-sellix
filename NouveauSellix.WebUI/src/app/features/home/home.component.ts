59192b9251131f9789341624bfbe8549e4eada1e
Gabriel Araújo <gabriel.araujo2902@outlook.com>
2025-05-25 19:37:58 -0300

    refactor: remove timer

--- NouveauSellix.WebUI/src/app/features/home/home.component.ts
+++ NouveauSellix.WebUI/src/app/features/home/home.component.ts
@@ -1,31 +1,12 @@
-import { DatePipe, isPlatformBrowser } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list'
 
 @Component({
   selector: 'app-home',
-  imports: [MatGridListModule, MatButtonModule, DatePipe],
+  imports: [MatGridListModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  protected readonly time = signal<Date>(new Date());
-
-  private intervalId: any;
-  private readonly platformId = inject(PLATFORM_ID);
-
-  public ngOnInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.intervalId = setInterval(() => {
-        this.time.set(new Date());
-      }, 1000);
-    }
-  }
-
-  public ngOnDestroy(): void {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-    }
-  }
+export class HomeComponent {
 }
