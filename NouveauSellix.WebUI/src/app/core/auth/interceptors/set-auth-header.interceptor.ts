afd16cd5a18eca59ed15f1c20e9c0405c22f0a9d
Gabriel Araújo <gabriel.araujo2902@outlook.com>
2025-05-25 19:26:35 -0300

    feat: trying req with loading

--- NouveauSellix.WebUI/src/app/core/auth/interceptors/set-auth-header.interceptor.ts
+++ NouveauSellix.WebUI/src/app/core/auth/interceptors/set-auth-header.interceptor.ts
@@ -2,13 +2,15 @@ import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/h
 import { inject } from '@angular/core';
 import { CurrentUserLoggedStore } from '../stores/current-user-logged-store.store';
 import { AuthTokenStorageService } from '../services/auth-token-storage.service';
-import { catchError, switchMap, throwError, of } from 'rxjs';
+import { catchError, switchMap, throwError, of, finalize } from 'rxjs';
 import { LoginUserFacade } from '../facades/login-user.facade';
+import { IsLoadingStore } from '../stores/is-loading-store.store';
 
 export const setAuthHeaderInterceptor: HttpInterceptorFn = (req, next) => {
   const isLoggedIn = inject(CurrentUserLoggedStore).isLoggedIn();
   const tokenStorage = inject(AuthTokenStorageService);
   const loginFacade = inject(LoginUserFacade);
+  const isLoading = inject(IsLoadingStore);
 
   const addAuthHeader = (token: string) =>
     req.clone({
@@ -24,6 +26,8 @@ export const setAuthHeaderInterceptor: HttpInterceptorFn = (req, next) => {
     return next(req);
   }
 
+  isLoading.startLoading();
+
   const requestWithAuth = isLoggedIn() ? addAuthHeader(tokenStorage.get()!) : req;
 
   return next(requestWithAuth).pipe(
@@ -34,6 +38,9 @@ export const setAuthHeaderInterceptor: HttpInterceptorFn = (req, next) => {
         switchMap(() => goToNext(err)),
         catchError(() => throwError(() => err))
       );
+    }),
+    finalize(() => {
+      isLoading.stopLoading();
     })
   );
 };
