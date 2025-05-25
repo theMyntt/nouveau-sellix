24c5b81325d1a1ce0e2a419993fb13b8e2d33ae4
Gabriel Araújo <gabriel.araujo2902@outlook.com>
2025-05-25 19:29:54 -0300

    fix: when signal changes value, re run function

--- NouveauSellix.WebUI/src/app/core/layout/layout.component.ts
+++ NouveauSellix.WebUI/src/app/core/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, effect, inject } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
@@ -12,18 +12,20 @@ import { LoadingComponent } from './components/loading/loading.component';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
   private readonly dialog = inject(MatDialog)
   private readonly isLoadingStore = inject(IsLoadingStore)
 
   protected readonly isLoading = this.isLoadingStore.get()
 
-  public ngOnInit(): void {
-    if (this.isLoading()) {
-      this.dialog.open(LoadingComponent)
-    }
-    else {
-      this.dialog.closeAll()
-    }
+  public constructor() {
+    effect(() => {
+      if (this.isLoading()) {
+        this.dialog.open(LoadingComponent)
+      }
+      else {
+        this.dialog.closeAll()
+      }
+    })
   }
 }
